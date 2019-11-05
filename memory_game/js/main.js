aef53592a250fa0582ec1f0b88acbe4e34ebda8e
combatmonk <gouthum@virtualnex.us>
2019-11-04 23:17:08 -0500

    96% done

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,26 @@
 
 const cards = ['queen','queen','king','king'];
 const cardsInPlay = [];
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardsInPlay);
 
-if (cardsInPlay.length === 2)
+/*Function for flipping card
+Step 1 Select card + push onto array
+Step 2 Select card + push onto array
+Step 3 Compare Array element for card one to array element for card two and log output
+*/
+
+function flipCard(){
+    cardOne = cards[0];
+    cardsInPlay.push(cardOne);
+    cardTwo = cards[1];
+    cardsInPlay.push(cardTwo);
+    if (cardsInPlay.length === 2)
     if (cardsInPlay[0] === cardsInPlay[1])
         alert("You found a match!");
     else
         alert('Sorry, try again');
+}
+console.log("User flipped " + cardsInPlay);
+
+
 
