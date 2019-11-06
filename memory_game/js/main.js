9c90e4603e62dbdade319ba004022558d8206a2b
Karadi <karadi@iMacked.local>
2019-11-06 06:45:46 -0500

    Improved Functions and Logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,19 +8,30 @@ Step 1 Select card + push onto array
 Step 2 Select card + push onto array
 Step 3 Compare Array element for card one to array element for card two and log output
 */
-
-function flipCard(){
-    cardOne = cards[0];
-    cardsInPlay.push(cardOne);
-    cardTwo = cards[1];
-    cardsInPlay.push(cardTwo);
-    if (cardsInPlay.length === 2)
-    if (cardsInPlay[0] === cardsInPlay[1])
-        alert("You found a match!");
-    else
-        alert('Sorry, try again');
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        console.log("You found a match!");
+    } else {
+        console.log("Sorry try again.");
+    }
 }
-console.log("User flipped " + cardsInPlay);
+
+function flipCard(cardId){
+    cardOne = cards[cardId];
+    cardsInPlay.push(cardOne);
+    cardId = Math.floor((Math.random() * 4) +1);
+    cardTwo = cards[cardId];
+    cardsInPlay.push(cardTwo);
+    console.log("User flipped " + cards[cardId]);
+    checkForMatch();
+}
+
+cardId = Math.floor((Math.random() * 4) +1);
+console.log(cardId);
+flipCard(cardId);
+
+
+
 
 
 
