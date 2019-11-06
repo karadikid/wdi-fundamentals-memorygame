e56049e3275627fa696129dc58d6e8bf8099ce12
KarateKid <goukaradi@hotmail.com>
2019-11-06 07:11:55 -0500

    Added objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,30 @@
 
-const cards = ['queen','queen','king','king'];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
+
 const cardsInPlay = [];
+
+
 
 
 /*Function for flipping card
@@ -17,12 +41,12 @@ function checkForMatch(){
 }
 
 function flipCard(cardId){
-    cardOne = cards[cardId];
+    cardOne = cards[cardId].rank;
     cardsInPlay.push(cardOne);
-    cardId = Math.floor((Math.random() * 4) +1);
-    cardTwo = cards[cardId];
+    cardId = Math.floor((Math.random() * (3 - 0)) +0);
+    cardTwo = cards[cardId].rank;
     cardsInPlay.push(cardTwo);
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
     checkForMatch();
 }
 
